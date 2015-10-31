27ac4cca76e8aac67a7bca16594b72ccc33cca5d
AnAtomInTheUniverse <amlan@umich.edu>
2015-10-31 19:03:25 -0400

    got most of the dependencies fixed -- need to debug

--- sdn_eval/sdn_eval.py
+++ sdn_eval/sdn_eval.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python
 
-
 # Imports
-import csv
+
 #import numpy as np
 #import matplotlib
 from google.protobuf import text_format
 import caffe 
-import caffe.draw
 from caffe.proto import caffe_pb2
 import argparse
+import sys
 
 #Iterate through all of the layers in the 
 #caffe model prototxt file and extract
@@ -20,19 +19,24 @@ def getLayerInfo(caffe_net):
 					print "Name: ", layer.name
 					print "Kernel Size: ", layer.convolution_param.kernel_size
 					print "Num Output: ", layer.convolution_param.num_output
+#enddef
 
 # Main procedure to read caffe model 
 if len(sys.argv) != 2:
 		print "Usage:", sys.argv[0], "Caffe Model File"
 		sys.exit(-1)
-
-		#Using argparse module to parse command line argument
-		#User must supply a valid '.prototxt' caffe model file 
-		parser 	=	argparse.ArgumentParser()
-		parser.add_argument('net_proto_file', help='Input Network prototxt file', type=argparse.FileType('r'))
-		args 	=	parser.parse_args()
-		net 	=	caffe_pb2.NetParameter()
-		text_format.Merge(open(args.net_proto_file).read(), net)
-		getLayerInfo(net)
+#endif
+print "About to read file " + sys.argv[1] + "\n" 
+#Using argparse module to parse command line argument
+#User must supply a valid '.prototxt' caffe model file 
+#parser 	=	argparse.ArgumentParser()
+#parser.add_argument('net_proto_file', help='Input Network prototxt file', type=argparse.FileType('r'))
+#args 	=	parser.parse_args()
+print "Got net param\n"
+model_file	=	sys.argv[1]
+net 		=	caffe_pb2.NetParameter()
+f			=	open(model_file, 'r')
+text_format.Merge(str(f.read()), net)
+getLayerInfo(net)
 
 
